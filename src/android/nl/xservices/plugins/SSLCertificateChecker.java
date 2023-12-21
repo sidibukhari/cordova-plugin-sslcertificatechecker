24f031080a664cbb8af25c7550959fe0ec835002
Sidi Bukhari <bukhari@infopro.com.my>
2023-12-21 11:00:25 +0800

    fix con.connect error

--- src/android/nl/xservices/plugins/SSLCertificateChecker.java
+++ src/android/nl/xservices/plugins/SSLCertificateChecker.java
@@ -29,7 +29,9 @@ public class SSLCertificateChecker extends CordovaPlugin {
             final JSONArray allowedFingerprints = args.getJSONArray(2);
             final String serverCertFingerprint = getFingerprint(serverURL);
             for (int j=0; j<allowedFingerprints.length(); j++) {
-              if (allowedFingerprints.get(j).toString().equalsIgnoreCase(serverCertFingerprint)) {
+              final String allowedFingerprint = allowedFingerprints.get(j).toString().trim();
+              allowedFingerprint = allowedFingerprint.replaceAll("\\s","");
+              if (allowedFingerprint.equalsIgnoreCase(serverCertFingerprint)) {
                 callbackContext.success("CONNECTION_SECURE");
                 return;
               }
@@ -51,7 +53,8 @@ public class SSLCertificateChecker extends CordovaPlugin {
   private static String getFingerprint(String httpsURL) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
     final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
     con.setConnectTimeout(5000);
-    con.connect();
+    con.getResponseCode();
+    // con.connect();
     final Certificate cert = con.getServerCertificates()[0];
     final MessageDigest md = MessageDigest.getInstance("SHA256");
     md.update(cert.getEncoded());
@@ -62,9 +65,9 @@ public class SSLCertificateChecker extends CordovaPlugin {
     final int n = data.length;
     final StringBuilder sb = new StringBuilder(n * 3 - 1);
     for (int i = 0; i < n; i++) {
-      if (i > 0) {
-        sb.append(' ');
-      }
+      // if (i > 0) {
+      //   sb.append(' ');
+      // }
       sb.append(HEX_CHARS[(data[i] >> 4) & 0x0F]);
       sb.append(HEX_CHARS[data[i] & 0x0F]);
     }
